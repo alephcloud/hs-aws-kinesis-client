5ed514c1c127e32a8bf644dd9d8b592dcdd2b24a
Jon Sterling <jon@jonmsterling.com>
2015-02-11 11:26:07 -0800

    import Aws.Kinesis.Commands.PutRecords
    I thought that this was already exported by Aws.Kinesis, but that must
    be from an unreleased version. Sorry for the inconvenience.

--- src/Aws/Kinesis/Client/Producer.hs
+++ src/Aws/Kinesis/Client/Producer.hs
@@ -72,6 +72,7 @@ module Aws.Kinesis.Client.Producer
 ) where
 
 import qualified Aws.Kinesis as Kin
+import qualified Aws.Kinesis.Commands.PutRecords as Kin
 import Aws.Kinesis.Client.Common
 
 import Control.Applicative
